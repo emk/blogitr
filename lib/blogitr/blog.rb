b937cc2be9821de360d33812fbe9744d23d35ade
Eric Kidd <git@randomhacks.net>
2009-04-19 13:59:40 -0400

    Blog: Allow querying for a single, known article
    This is needed to implement each article's permalink page.

--- lib/blogitr/blog.rb
+++ lib/blogitr/blog.rb
@@ -1,16 +1,14 @@
 require 'yaml'
 
 module Blogitr
+  # This error is raised if an article does not appear to exist.
+  class ArticleNotFoundError < RuntimeError
+  end
+
   # The main interface to the Blogitr module.  This class represents a
   # single blog.  Unless indicated otherwise, articles are always returned
   # in reverse-chronological order.
   class Blog
-    YEAR_GLOB    = "[0-9][0-9][0-9][0-9]"     # :nodoc:
-    MONTH_GLOB   = "[0-9][0-9]"               # :nodoc:
-    DAY_GLOB     = "[0-9][0-9]"               # :nodoc:
-    TIME_GLOB    = "[0-9][0-9][0-9][0-9]"     # :nodoc:
-    ARTICLE_GLOB = "#{TIME_GLOB}-*.*[a-z0-9]" # :nodoc: (reject "*.textile~")
-
     # Create a new blog object using the data stored at +root+.
     def initialize root
       @root = root
@@ -23,11 +21,48 @@ module Blogitr
     end
 
     # All the articles posted to the blog.
-    def articles
-      glob = File.join(@root, YEAR_GLOB, MONTH_GLOB, DAY_GLOB, ARTICLE_GLOB)
+    #
+    # SECURITY: We should validate each of these values before checking
+    # file system.  But for now, we rely on our caller to get it right.
+    def articles year=nil, month=nil, day=nil, permalink=nil
+      # Build a glob fragment for each of our path components.
+      year_glob      = digit_glob(year, 4)
+      month_glob     = digit_glob(month, 2)
+      day_glob       = digit_glob(day, 2)
+      hhmm_glob      = digit_glob(nil, 4)
+      filter_glob    = "*[a-z0-9]" # (reject "*.textile~")
+      permalink_glob = permalink || "*"
+
+      # A poor-man's database query.
+      glob = File.join(@root, year_glob, month_glob, day_glob,
+                       "#{hhmm_glob}-#{permalink_glob}.#{filter_glob}")
       Dir[glob].sort.reverse.map do |path|
         relative_path = path[(@root+"/").length..-1]
         Blogitr::Article.new(:path => relative_path, :text => File.read(path))
+      end
+    end
+
+    # Return the specified article, or raise ArticleNotFoundError if the
+    # article does not exist.
+    def article year, month, day, permalink
+      result = articles(year, month, day, permalink)
+      unless result.length == 1
+        raise(ArticleNotFoundError,
+              "Can't find #{year}/#{month}/#{day}/#{permalink}")
+      end
+      result[0]
+    end
+
+    private
+
+    # If +value+ is not +nil+, convert it to a string and pad it on the
+    # left with 0s until it is +digits+ characters long.  If +value+ is
+    # +nil+, return a glob which matches that many digits.
+    def digit_glob value, digits
+      if value
+        value.to_s.rjust(digits, '0')
+      else
+        "[0-9]" * digits
       end
     end
   end
--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -14,4 +14,15 @@ describe Blogitr::Blog do
     articles[0].title.should == "My newest post"
     articles[1].title.should == "My second-newest post"
   end
+
+  it "should allow searching for an individual article" do
+    article = @blog.article(2009, 4, 19, 'my-second-newest-post')
+    article.title.should == "My second-newest post"
+  end
+
+  it "should raise an error if an article can't be found" do
+    lambda do
+      article = @blog.article(2009, 4, 19, 'does-not-exist')
+    end.should raise_error(Blogitr::ArticleNotFoundError)
+  end
 end
