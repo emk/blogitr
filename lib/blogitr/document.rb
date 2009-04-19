683588f991193d51d0164a1df86087c4d0e6dd68
Eric Kidd <git@randomhacks.net>
2009-04-19 08:00:07 -0400

    Document: Factor parsing code out of initialize

--- lib/blogitr/document.rb
+++ lib/blogitr/document.rb
@@ -21,41 +21,53 @@ module Blogitr
     attr_reader :body
 
     # The portion of the document appearing after the first line containing
-    # \<!--more-->, if any.
+    # \<!--more-->, if any.  May be +nil+.
     attr_reader :extended
 
-    # Parse +string+ into headers, body and extended content, and apply an
-    # optional +filter+ from Blogitr::FILTERS to the body and extended
-    # content.
-    def initialize string, filter=:html
-      # Look up our text filter.
-      @filter = Blogitr.find_filter(filter)
-
+    # Parse +string+ into headers, body and extended content, and return a
+    # Document.
+    def self.parse string, filter
       # Extract our headers, if any.
       if string =~ /\A.*:/
         if string =~ /\A((?:.|\n)*?)\n\n((?:.|\n)*)\z/
           # Headers and body.
-          @headers = YAML::load($1)
+          headers = YAML::load($1)
           content = $2
         else
           # Headers without body.
-          @headers = YAML::load(string)
+          headers = YAML::load(string)
           content = ''
         end
       else
         # Body without headers.
-        @headers = {}
+        headers = {}
         content = string
       end
 
       # Split the body at a <!--more--> marker, if we have one.
       if content =~ /\A((?:.|\n)*?)<!--more-->\s*\n((?:.|\n)*)\z/
-        @body = $1
-        @extended = $2
+        body = $1
+        extended = $2
       else
-        @body = content
-        @extended = nil
-      end
+        body = content
+        extended = nil
+      end      
+
+      # Create our Document.
+      new headers, body, extended, filter
+    end
+
+    # Construct a new Document, using +headers+, +body+ and +extended+, and
+    # use +filter+ to process +body+ and +extended+.  +headers+ should be a
+    # hash table, and +filter+ should be a symbol corresponding to a
+    # registered Filter.
+    def initialize headers, body, extended, filter
+      @headers = headers
+      @body = body
+      @extended = extended
+
+      # Look up our text filter.
+      @filter = Blogitr.find_filter(filter)
 
       # Apply macros and filters to our content.
       @body = process_text(body)
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Blogitr::Document do
+  def parse text, filter=:html
+    @doc = Blogitr::Document.parse text, filter
+  end
+
   def should_parse_as headers, body, extended=nil
     @doc.headers.should == headers
     @doc.body.should == body
@@ -8,7 +12,7 @@ describe Blogitr::Document do
   end
 
   it "should parse documents with a YAML header" do
-    @doc = Blogitr::Document.new <<EOD
+    parse <<EOD
 title: My Doc
 subtitle: An Essay
 
@@ -21,17 +25,17 @@ EOD
   end
 
   it "should parse documents without a YAML header" do
-    @doc = Blogitr::Document.new "foo\nbar\nbaz"
+    parse "foo\nbar\nbaz"
     should_parse_as({}, "foo\nbar\nbaz")
   end
 
   it "should parse documents with a YAML header but no body" do
-    @doc = Blogitr::Document.new("title: My Doc")
+    parse "title: My Doc"
     should_parse_as({ 'title' => "My Doc" }, '')
   end
 
   it "should separate extended content from the main body" do
-    @doc = Blogitr::Document.new <<EOD
+    parse <<EOD
 foo
 <!--more--> 
 bar
@@ -41,40 +45,40 @@ EOD
 
   it "should expand macros" do
     input = "title: Foo\n\n<macro:example foo=\"bar\">baz</macro:example>"
-    @doc = Blogitr::Document.new(input)
+    parse input
     should_parse_as({'title' => "Foo"},
                     "Options: {\"foo\"=>\"bar\"}\nBody: baz")
   end
 
   it "should raise an error if an unknown fitler is specified" do
     lambda do
-      @doc = Blogitr::Document.new("foo *bar* \"baz\"", :unknown)
+      parse "foo *bar* \"baz\"", :unknown
     end.should raise_error(Blogitr::UnknownFilterError)
   end
 
   describe "with a :textile filter" do
     it "should filter content" do
-      @doc = Blogitr::Document.new("foo *bar*\n<!--more-->\n\"baz\"", :textile)
+      parse "foo *bar*\n<!--more-->\n\"baz\"", :textile
       should_parse_as({}, "<p>foo <strong>bar</strong></p>",
                       "<p>&#8220;baz&#8221;</p>")
     end
 
     it "should protect expanded macros from filtering" do
       text = "\n<macro:example>*foo*</macro:example>"
-      @doc = Blogitr::Document.new(text, :textile)
+      parse text, :textile
       should_parse_as({},  "Options: {}\nBody: *foo*")
     end
   end
 
   describe "with a :markdown filter" do
     it "should filter content" do
-      @doc = Blogitr::Document.new("foo *bar* \"baz\"", :markdown)
+      parse "foo *bar* \"baz\"", :markdown
       should_parse_as({}, "<p>foo <em>bar</em> &ldquo;baz&rdquo;</p>\n")
     end
 
     it "should protect expanded macros from filtering" do
       text = "\n<macro:example>*foo*</macro:example>"
-      @doc = Blogitr::Document.new(text, :markdown)
+      parse text, :markdown
       should_parse_as({},
                       "<div class=\"raw\">Options: {}\nBody: *foo*</div>\n\n")
     end
